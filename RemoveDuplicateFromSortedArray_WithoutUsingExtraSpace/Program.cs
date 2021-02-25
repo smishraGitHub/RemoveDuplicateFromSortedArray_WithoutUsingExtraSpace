c744872c9945480c00b71f8a46e15d9b03024441
saurabh mishra <smishra43@hotmail.com>
2021-02-25 12:35:42 +0530

    Add project files.

---
+++ RemoveDuplicateFromSortedArray_WithoutUsingExtraSpace/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicateFromSortedArray_WithoutUsingExtraSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 5 };
+           // int[] nums = new int[] { 0, 0, 1 };
+           
+
+           // if (nums.Length == 0) return 0;
+            int i = 0;
+
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+
+            int output= i + 1;
+        }
+    }
+}
